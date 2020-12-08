cb7d909c9f4811c89675cb0ba40f660c55b07d59
Ivan Dimitrov <biobeehive@abv.bg>
2020-12-08 05:16:38 +0200

    Initial Commit

---
+++ MusicNewSnow/MusicNewSnowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicNewSnowApp.swift
+//  MusicNewSnow
+//
+//  Created by Ivan Dimitrov on 8.12.20.
+//
+
+import SwiftUI
+
+@main
+struct MusicNewSnowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
